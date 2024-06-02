1a8ab00227f2a1e4602bdc36ef616451549a4e23
murphyqwek <qwekinaft@list.ru>
2024-06-02 15:13:57 +0700

    Added ToolTip for ExtraData Field

---
+++ TestMODBUS/Extensions/ExtraDataFieldTooltipText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestMODBUS.Extensions
+{
+    internal class ExtraDataFieldTooltipText
+    {
+        public static readonly DependencyProperty ExtraDataFieldTooltipTextProperty = DependencyProperty.RegisterAttached(
+            "ExtraDataFieldTooltipText",
+            typeof(string),
+            typeof(ExtraDataFieldTooltipText),
+            new PropertyMetadata(""));
+
+        public static void SetExtraDataFieldTooltipText(UIElement element, string value)
+        {
+            element.SetValue(ExtraDataFieldTooltipTextProperty, value);
+        }
+
+        public static string GetExtraDataFieldTooltipText(UIElement element)
+        {
+            return (string)element.GetValue(ExtraDataFieldTooltipTextProperty);
+        }
+    }
+}
