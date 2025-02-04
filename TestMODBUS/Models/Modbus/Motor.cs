44dbea244e52d901fe51743193191735f6d16bf8
murphyqwek <qwekinaft@list.ru>
2025-02-04 17:33:31 +0700

    test

--- TestMODBUS/Models/Modbus/Motor.cs
+++ TestMODBUS/Models/Modbus/Motor.cs
@@ -18,25 +18,28 @@ namespace ModBusTPU.Models.Modbus
         private DataStorage dataStorage;
         private bool working = false;
 
-        static string PORT = "COM8";
+        static string PORT = "COM9";
         static int BAUDRATE = 9600;
         static byte ADDRESS = 1;
 
         static SerialPort serialPort;
         static ModbusSerialMaster master;
-        static double currentThreshold = 150; // Уставка по току
+        static double currentThreshold = 125; // Уставка по току
         static double voltageThreshold = 40; // Уставка по напряжению
 
-        const double highCurrentThresholdBound = 5;
-        const double lowCurrentThresholdBound = 5;
+        const double highCurrentThresholdBound = 1;
+        const double lowCurrentThresholdBound = 1;
 
         const double highVoltageThresholdBound = 5;
         const double lowVoltageThresholdBound = 5;
 
-        const int WRITEDELAY = 200;
-        const int ITERATIONDELAY = 100;
+        const int KZDELAY = 5000;
+
+        const int WRITEDELAY = 5;
+        const int ITERATIONDELAY = 3;
         const int REVERSDELAY = 3000;
-        const int SPEED = 200;
+        const int REVERSESPEED = 500;
+        const int SPEED = 100;
 
         public Motor(DataStorage dataStorage)
         {
@@ -68,11 +71,33 @@ namespace ModBusTPU.Models.Modbus
         public void ControlMotor()
         {
             WriteRegister(0x000F, 1);
+
+            double current = 0;
+            double voltage = 0;
+
+            WriteRegister(0x0103, 50);
+            Console.WriteLine("Вниз до кз");
+            while (current < 145 && working)
+            {
+                WriteRegister(0x0105, SPEED);
+                WriteRegister(0x0100, 1);
+
+                lock (this)
+                {
+                    current = dataStorage.GetChannelData(0).Last().Y;
+                    voltage = dataStorage.GetChannelData(5).Last().Y;
+                }
+
+                current = ModBusValueConverter.ConvertToAmperValue(current);
+                voltage = ModBusValueConverter.ConvertToVoltValue(voltage);
+                Console.WriteLine($"{current} {voltage}");
+            }
+            Console.WriteLine("КЗ!!!!");
+            WriteRegister(0x0100, 3);
+            Thread.Sleep(KZDELAY);
+            Console.WriteLine("KZDELAY прошел");
             while (working)
             {
-                double current = 0;
-                double voltage = 0;
-
                 lock (this) {
                     current = dataStorage.GetChannelData(0).Last().Y;
                     voltage = dataStorage.GetChannelData(5).Last().Y;
@@ -84,12 +109,12 @@ namespace ModBusTPU.Models.Modbus
                 if (current < currentThreshold - lowCurrentThresholdBound)
                 {
                     WriteRegister(0x0105, SPEED);
-                    WriteRegister(0x0100, 0);
+                    WriteRegister(0x0100, 1);
                 }
                 else if (current > currentThreshold + highCurrentThresholdBound)
                 {
                     WriteRegister(0x0105, SPEED);
-                    WriteRegister(0x0100, 1);
+                    WriteRegister(0x0100, 0);
                 }
                 else
                 {
@@ -114,8 +139,10 @@ namespace ModBusTPU.Models.Modbus
                 }*/
                 Thread.Sleep(ITERATIONDELAY);
             }
-            WriteRegister(0x0105, SPEED);
-            WriteRegister(0x0100, 1);
+
+            WriteRegister(0x0103, 10000);
+            WriteRegister(0x0105, REVERSESPEED);
+            WriteRegister(0x0100, 0);
             Thread.Sleep(REVERSDELAY);
             WriteRegister(0x0100, 3);
 
@@ -132,6 +159,7 @@ namespace ModBusTPU.Models.Modbus
                 //Console.WriteLine("Соединение установлено! Начинаем контроль параметров...");
                 var listenningThread = new Thread(() => ControlMotor());
                 working = true;
+                //Console.WriteLine(ReadRegister(0x0101));
                 listenningThread.Start();
             }
             catch (Exception ex)
