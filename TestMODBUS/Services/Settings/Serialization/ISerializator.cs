b60d4fc9445a8af77616749b76964e2e17bfce16
murphyqwek <qwekinaft@list.ru>
2025-03-19 19:37:31 +0300

    Add ISerializator interface

---
+++ TestMODBUS/Services/Settings/Serialization/ISerializator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModBusTPU.Services.Settings.Serialization
+{
+    internal interface ISerializator<T>
+    {
+        string Serialize(T serialazable);
+
+        T Deserialize(string serialazable);
+    }
+}
