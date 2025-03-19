8f91057ba503aab0cc2245491ca1ec4c55c509e5
murphyqwek <qwekinaft@list.ru>
2025-03-19 19:35:51 +0300

    Add Save, Upload, Serialize, Deserialize, GetDefaultSettings to SettingsManager

---
+++ TestMODBUS/Services/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModBusTPU.Services
+{
+    internal class Result<T>
+    {
+        public T Value { get; set; }
+        public string ErrorMessage { get; set; }
+        public bool IsSuccess => string.IsNullOrEmpty(ErrorMessage);
+
+        private Result(T value, string errorMessage)
+        {
+            Value = value;
+            ErrorMessage = errorMessage ?? string.Empty;
+        }
+
+        public static Result<T> Success(T value) => new Result<T>(value, string.Empty);
+        public static Result<T> Failure(string errorMessage) => new Result<T>(default, errorMessage);
+    }
+}
--- TestMODBUS/Services/Settings/SettingsManager.cs
+++ TestMODBUS/Services/Settings/SettingsManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -25,6 +26,79 @@ namespace ModBusTPU.Services.Settings
             _configPath = configPath;
         }
 
-        
+        /// <summary>
+        /// Метод для загрузки файла настроек
+        /// </summary>
+        /// <returns>Возвращает SettingsContainer и текст ошибки, если она произошла. Если файл загружен успено, текст ошибки будет равен string.Empty. Если не удалось загрузить файл, то SettingsContainer будет иметь стандартные настройки</returns>
+        public (SettingsContainer settings, string exceptionTest) Upload()
+        {
+            if(!File.Exists(ConfigPath))
+            {
+                try
+                {
+                    var Settings = new SettingsContainer();
+                    Save(Settings);
+                }
+                catch (IOException)
+                {
+                    return (GetDefaultSettings(), "Не удалось открыть файл");
+                }
+                catch (Exception ex)
+                {
+                    return (GetDefaultSettings(), "Произошла непредвиденная ошибка: " + ex.Message);
+                }
+            }
+
+            try
+            {
+                string json = File.ReadAllText(ConfigPath);
+                var settings = Deserialize(json);
+
+                if (settings == null)
+                {
+                    return (GetDefaultSettings(), "Не удалось десериализовать настройки. Возвращены дефолтные значения.");
+                }
+
+                return (settings, string.Empty);
+            }
+            catch (IOException)
+            {
+                return (GetDefaultSettings(), "Не удалось открыть файл");
+            }
+            catch (Exception ex)
+            {
+                return (GetDefaultSettings(), "Произошла непредвиденная ошибка: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Метод для сохранения настроек
+        /// </summary>
+        /// <param name="settings"> Контейнер с настройками </param>
+        public void Save(SettingsContainer settings)
+        {
+            string json = Serialize(settings);
+            File.WriteAllText(json, ConfigPath);
+        }
+
+        /// <summary>
+        /// Метод для получения стандартных настроек
+        /// </summary>
+        private SettingsContainer GetDefaultSettings()
+        {
+            return new SettingsContainer();
+        }
+
+        private SettingsContainer Deserialize(string serializedSettings)
+        {
+            var settings = JsonSerializer.Deserialize<SettingsContainer>(serializedSettings, JsonSerializerOptions);
+            return settings;
+        }
+
+        private string Serialize(SettingsContainer settings)
+        {
+            string json = JsonSerializer.Serialize(settings, JsonSerializerOptions);
+            return json;
+        }
     }
 }
