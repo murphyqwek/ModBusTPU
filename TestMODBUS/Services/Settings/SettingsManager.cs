5aff36be5f1fdabb007054fe97a55b06128f44ec
murphyqwek <qwekinaft@list.ru>
2025-03-19 19:02:14 +0300

    Create SettingManager class

---
+++ TestMODBUS/Services/Settings/SettingsManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ModBusTPU.Services.Settings
+{
+    internal class SettingsManager
+    {
+        private string _configPath;
+
+        private static JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions()
+        {
+            WriteIndented = true, //Красивый формат
+            PropertyNameCaseInsensitive = true, // Позволяет игнорировать регистр полей
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull // Игнорирует null-поля
+        };
+
+        public string ConfigPath { get { return _configPath; } }
+
+        public SettingsManager(string configPath)
+        {
+            _configPath = configPath;
+        }
+
+        
+    }
+}
