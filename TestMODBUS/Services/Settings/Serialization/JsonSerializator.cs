30f37865e1e0c54d992c919dcb0066865f2ecde7
murphyqwek <qwekinaft@list.ru>
2025-03-19 19:39:38 +0300

    Add JsonSerializator

---
+++ TestMODBUS/Services/Settings/Serialization/JsonSerializator.cs
@@ -0,0 +1,33 @@
+﻿using ModBusTPU.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ModBusTPU.Services.Settings.Serialization
+{
+    internal class JsonSerializator<T> : ISerializator<T>
+    {
+
+        private static JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions()
+        {
+            WriteIndented = true, //Красивый формат
+            PropertyNameCaseInsensitive = true, // Позволяет игнорировать регистр полей
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull // Игнорирует null-поля
+        };
+
+        public T Deserialize(string serialazable)
+        {
+            var settings = JsonSerializer.Deserialize<T>(serialazable, JsonSerializerOptions);
+            return settings;
+        }
+
+        public string Serialize(T serialazable)
+        {
+            string json = JsonSerializer.Serialize(serialazable, JsonSerializerOptions);
+            return json;
+        }
+    }
+}
